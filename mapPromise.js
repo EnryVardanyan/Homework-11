529bc9fea5fcb91197e1b75bf573e3790726620c
Enry Vardanyan <Vardanyan.enry7@gmail.com>
2023-11-04 01:37:50 -0700

    ex 5 added

--- mapPromise.js
+++ mapPromise.js
@@ -4,3 +4,21 @@ function mapPromise(promise, transformer){
 const myPromise = Promise.resolve(10)
 const myTransformer = (val => val * 2)
 mapPromise(myPromise, myTransformer).then(res => console.log(res)).catch(err => console.log(err))
+
+//ex 5  Promise.resolve()
+// .then(() => console.log(1))
+// .catch(() => console.log(3))
+// .then(() => { console.log(2); throw new Error(); })
+// .then(() => console.log(4))
+// .then(() => console.log(4))
+// 1,2,error
+
+
+// Promise.resolve()
+// .then(() => console.log(11))
+// .then(() => { console.log(12); throw new Error(); })
+// .catch(() => console.log(13))
+// .catch(() => console.log(14))
+// .then(() => console.log(15))
+
+// 11,12,13,15
